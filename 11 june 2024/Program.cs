c4ee84687fa3a2e03d99d00dfed7e05bb606e3a3
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-11 06:59:16 +1200

    Add project files.

---
+++ 11 june 2024/Program.cs
@@ -0,0 +1,16 @@
+﻿//guess the number software
+//creating a instance of the random class
+Random random = new Random();
+int randomNumber = random.Next(0, 10);
+Console.WriteLine("Guess the number");
+int userNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("computer guessed " + randomNumber);
+if(randomNumber == userNumber)
+{
+    Console.WriteLine("you guessed right");
+  
+}
+else
+{
+    Console.WriteLine("Try again");
+}
